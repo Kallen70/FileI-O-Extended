32a51acbf36ba5603c4c2808f31244938d20374c
Kevin Allen <kevinallen@Kevins-MacBook-Air.local>
2017-04-09 15:34:06 -0400

    First Commit

--- src/com/theironyard/novauc/Main.java
+++ src/com/theironyard/novauc/Main.java
@@ -1,8 +1,79 @@
 package com.theironyard.novauc;
 
-public class Main {
+import jodd.json.JsonSerializer;
 
-    public static void main(String[] args) {
-	// write your code here
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    static ArrayList<Person> people = new ArrayList<>();
+    static HashMap<String, ArrayList<Person>> listMap = new HashMap<>();
+
+
+    public static void readFile() throws FileNotFoundException {
+
+        File f = new File("people.txt");
+        Scanner scanner = new Scanner(f);
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            String[] columns = line.split(",");
+            Person person = new Person(String.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);
+            people.add(person);
+
+
+        }
+
+
     }
+
+    public static void loadListMap() {
+        for (Person person : people) {
+
+            String country = person.country;
+            ArrayList<Person> peopleFromCountry = listMap.get(country);
+
+            if (peopleFromCountry == null) {
+                peopleFromCountry = new ArrayList<>();
+
+            }
+            peopleFromCountry.add(person);
+            listMap.put(country, peopleFromCountry);
+
+        }
+
+
+    }
+    public static void printListMap()  {
+        System.out.println(listMap);
+    }
+    public static  void writeToFile() throws IOException {
+        File f = new File("people.json");
+           JsonSerializer serializer = new JsonSerializer();
+        String json = serializer.include("*").serialize(listMap);
+        FileWriter fw = new FileWriter(f);
+        fw.write(json);
+        fw.close();
+
+
+    }
+
+    public static void printPerson(){
+
+    }
+
+
+    public static void main (String[] args) throws IOException{
+
+        readFile();
+        loadListMap();
+        printListMap();
+        writeToFile();
+        printPerson();
+    }
+
 }
